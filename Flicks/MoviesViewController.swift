93475c2710664397cb48a7cd128403a0d1094017
vczhou <victoria.ch.zhou@gmail.com>
2017-01-29 00:20:57 -0600

    Basic table view set up

---
+++ Flicks/MoviesViewController.swift
@@ -0,0 +1,49 @@
+//
+//  MoviesViewController.swift
+//  Flicks
+//
+//  Created by Victoria Zhou on 1/28/17.
+//  Copyright (c) 2017 Victoria Zhou. All rights reserved.
+//
+
+import UIKit
+
+class MoviesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("MovieCell", forIndexPath: indexPath)
+        cell.textLabel!!.text = "row \(indexPath.row)"
+        print("row \(indexPath.row)")
+        return cell as! UITableViewCell
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
