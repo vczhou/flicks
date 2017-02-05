aeac564bd75189ff939ede3d89f9f77b255cc6c1
vczhou <victoria.ch.zhou@gmail.com>
2017-02-05 00:50:34 -0600

    added scroll view

--- Flicks/DetailViewController.swift
+++ Flicks/DetailViewController.swift
@@ -13,17 +13,22 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var posterImageView: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var overviewLabel: UILabel!
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var infoView: UIView!
     
     var movie: NSDictionary!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        scrollView.contentSize = CGSize(width: scrollView.frame.size.width, height: infoView.frame.origin.y + infoView.frame.size.height)
+        
         let title = movie["title"] as! String
         titleLabel.text = title
         
         let overview = movie["overview"] as! String
         overviewLabel.text = overview
+        overviewLabel.sizeToFit()
         
         let baseURL = "https://image.tmdb.org/t/p/w500"
         if let posterPath = movie["poster_path"] as? String {
